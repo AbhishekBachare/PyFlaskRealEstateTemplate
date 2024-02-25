c4c5c64f03c654aa327d721752bcd59070d40f25
AbhishekBachare <abhishekbachare@gmail.com>
2024-02-25 13:05:52 +0530

    4th main Commit: Moved styles to /static/css/styles.css and Added pics to /static/pics.

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-# app.py
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define routes
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/estate')
 def estate():
-    # You can replace this with logic to fetch and display real estate pictures
     estate_pics = [
-        'pic1.jpg',
-        'pic2.jpg',
-        'pic3.jpg',
+        'static/pics/pic1.png',
+        'static/pics/pic2.png',
+        'static/pics/pic3.png',
     ]
     return render_template('estate.html', estate_pics=estate_pics)
 
